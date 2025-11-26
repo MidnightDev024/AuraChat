f528b3b8cdffc4edf4c249053f61a1e19f73f84f
MidnightDev024 <chawlaakash024@gmail.com>
2025-11-26 23:55:28 +0530

    refactored : userRoutes.js(server)

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, checkAuth, updateProfile } from '../controllers/userController.js';
+import { signup, login, checkAuth, updateProfile, getVapidKey, savePushSubscription, removePushSubscription } from '../controllers/userController.js';
 import { protectRoute } from '../middleware/auth.js';
 
 const userRouter = express.Router()
@@ -8,5 +8,10 @@ userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.get("/check", protectRoute, checkAuth);
 userRouter.put("/updateProfile", protectRoute, updateProfile);
+
+// Push notification endpoints
+userRouter.get("/vapid-key", getVapidKey);
+userRouter.post("/push-subscription", protectRoute, savePushSubscription);
+userRouter.delete("/push-subscription", protectRoute, removePushSubscription);
 
 export default userRouter;
